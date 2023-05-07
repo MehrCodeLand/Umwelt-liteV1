dd00a921698d536b4c056ba555120e3405b80fea
MehrCodeLand <mehrcodeland@gmail.com>
2023-05-07 16:17:23 +0330

    time to save image.

--- Umwelt-liteV/Core/Repositories/AdminRepository.cs
+++ Umwelt-liteV/Core/Repositories/AdminRepository.cs
@@ -1,4 +1,5 @@
-﻿using Umwelt_liteV.Core.Services;
+﻿using System.Text.RegularExpressions;
+using Umwelt_liteV.Core.Services;
 using Umwelt_liteV.Creator;
 using Umwelt_liteV.Data.Models.Entities;
 using Umwelt_liteV.Data.Models.Structs;
@@ -112,17 +113,122 @@ namespace Umwelt_liteV.Core.Repositories
             var message = new MessageData();
 
             message = ValidateCreateArticle(articleVm);
+            if(message.ErrorId < 0 ) { return message; }
+            
+            // time to save data 
+            var article = new Article()
+            {
+                MyArticleId = CreateRandomId.CreateId(),
+                Title = articleVm.Title.ToLower(),
+                ShortDescriptions = articleVm.ShortDiscription,
+                Descriptions = articleVm.Discrioption,
+                ImageName = articleVm.Title.ToLower(),
+                CategoryId = articleVm.CategoryID,
+            };
+
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/articleData/image", article.ImageName);
 
             return message;
         }
         private MessageData ValidateCreateArticle(CreateArticleVm articleVm)
         {
+
+
             var message = new MessageData();
 
+            if (articleVm == null)
+            {
+                message.ErrorId = -100;
+                message.Message = "Data is not find.";
+                return message;
+            }
+            else if ((articleVm.Title.Length < 2) ||(articleVm.Title == null) || (articleVm.Title.Length > 20))
+            {
+                message.ErrorId = -110;
+                message.Message = "Title is not correct structures";
+
+                return message;
+            }
+
+            var regex = new Regex("^[a-zA-Z]");
+            if (!regex.IsMatch(articleVm.Title))
+            {
+                message.ErrorId = -130;
+                message.Message = "Title has not number!";
+
+                return message;
+            }
+
+            articleVm.Title = articleVm.Title.Replace(" ", "");
+
+
+            if((articleVm.ShortDiscription == null) || (articleVm.ShortDiscription.Length < 15) || 
+                (articleVm.ShortDiscription.Length > 25 ))
+            {
+                message.ErrorId = -140;
+                message.Message = "Short description has not correct\n" +
+                    " structures ";
+
+                return message;
+            } 
+            else if((articleVm.Discrioption == null ) || (articleVm.Discrioption.Length < 30 ))
+            {
+                message.ErrorId = -160;
+                message.Message = "Descriptions has incorrect structures ";
+
+                return message;
+            }
+            else if(articleVm.Discrioption == null)
+            {
+                message.ErrorId = -175;
+                message.Message = "Fill Iamge";
+
+                return message;
+            }
+            else if(articleVm.ImageFile == null || articleVm.ImageFile.Length > 2097152)
+            {
+                message.ErrorId = -160;
+                message.Message = "thats large image";
+
+                return message;
+            }
+            else if(!ValidateImageFormat(articleVm.ImageFile.ContentType))
+            {
+                message.ErrorId = -300;
+                message.Message = "incorect image format jpg or png need";
+                return message;
+            }
+            else if (!IsCategoryExist(articleVm.CategoryID))
+            {
+                message.ErrorId = -210;
+                message.Message = "This categor is not exist";
+
+                return message;
+            }
+            else if(articleVm.CategoryID.GetType() != typeof(int))
+            {
+                message.ErrorId = -240;
+                message.Message = "we have some problem with cat id ";
+
+                return message;
+            }
+
+            message.SuccessId = 100;
+            message.Message = "All is done";
             return message;
+        }        
+        private bool ValidateImageFormat(string fileContentType)
+        {
+
+            if(fileContentType == "image/png") { return true; }
+            else if(fileContentType == "image/jpg") { return true; }
+
+            return false;
         }
-
-
+        private bool IsCategoryExist( int catId)
+        {
+            return _db.Categories.Any(u => u.CategoryId == catId);
+        }
 
         #endregion
 
